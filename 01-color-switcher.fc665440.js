179b6472792283174a7d9e3317a126ec1c652241
Oleksii-co <110523568+Oleksii-co@users.noreply.github.com>
2023-01-31 19:20:23 +0000

    Deploying to gh-pages from @ Oleksii-co/goit-js-hw-09@0962010e54d902d5dfff6bd2a7c547d796c8d11e 🚀

---
+++ 01-color-switcher.fc665440.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");t.disabled=!0;let d=null;function o(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(function(){document.body.style.backgroundColor=`${o()}`,d=setInterval((()=>document.body.style.backgroundColor=`${o()}`),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){document.body.style.backgroundColor="#fafafa",clearInterval(d),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.fc665440.js.map
